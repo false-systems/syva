4ad852575b324e7cde2082431857eee131abb98f
Yair Etziony <yair.etziony@gmail.com>
2026-04-20 01:43:07 +0200

    fix(syva-proto): wire google.protobuf well-known types for ZoneService
    Enables import of google/protobuf/timestamp.proto required by
    syva_control.proto. Uses prost-types for generated timestamps.

--- syva-proto/build.rs
+++ syva-proto/build.rs
@@ -2,6 +2,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .build_client(true)
+        .compile_well_known_types(true)
+        .extern_path(".google.protobuf", "::prost_types")
         .compile_protos(
             &["proto/syva_core.proto", "proto/syva_control.proto"],
             &["proto/"],
