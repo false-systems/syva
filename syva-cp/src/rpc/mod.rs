04a74987faf10b06b5191ed60cb8861e7f43c157
Yair Etziony <yair.etziony@gmail.com>
2026-04-24 01:24:08 +0200

    feat(syva-cp): AssignmentService with streaming subscribe and report

--- syva-cp/src/rpc/mod.rs
+++ syva-cp/src/rpc/mod.rs
@@ -8,17 +8,27 @@ use std::net::SocketAddr;
 use tokio::task::JoinHandle;
 use tonic::transport::Server;
 
+pub mod assignment_service;
 pub mod node_service;
 pub mod team_service;
 pub mod zone_service;
 
-pub async fn spawn(pool: PgPool, _bus: AssignmentBus, addr: SocketAddr) -> Result<JoinHandle<()>> {
+pub async fn spawn(pool: PgPool, bus: AssignmentBus, addr: SocketAddr) -> Result<JoinHandle<()>> {
+    let assignment_svc = assignment_service::AssignmentServiceImpl {
+        pool: pool.clone(),
+        bus,
+    };
     let node_svc = node_service::NodeServiceImpl { pool: pool.clone() };
     let team_svc = team_service::TeamServiceImpl { pool: pool.clone() };
     let zone_svc = zone_service::ZoneServiceImpl { pool: pool.clone() };
 
     let handle = tokio::spawn(async move {
         let result = Server::builder()
+            .add_service(
+                syva_proto::syva_control::v1::assignment_service_server::AssignmentServiceServer::new(
+                    assignment_svc,
+                ),
+            )
             .add_service(
                 syva_proto::syva_control::v1::node_service_server::NodeServiceServer::new(
                     node_svc,
